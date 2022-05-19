f279314a071a429169e5bcf6890a225b17f3fbb0
Juho Kangas <jkangas@student.hive.fi>
2022-05-19 11:32:13 +0300

    add 02

---
+++ scripts/02.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "-------------------------"
+echo "DELETING A USER"
+echo "-------------------------"
+echo
+echo
+echo "List of active users:"
+users
+while :
+do
+    echo "Which user to delete?"
+    read user
+    if [ $user != 'root' ]
+    then
+        break
+    else
+        echo "Don't be silly..."
+    fi
+done
+
+read -p "Deleting active user will leave the system to an inconsistent state. Are you sure? (y/n) " yes_no
+
+if [ $yes_no == 'y' ]
+then
+    sudo userdel -rf $user
+fi
+exit#!/bin/bash
+
+echo "-------------------------"
+echo "DELETING A USER"
+echo "-------------------------"
+echo
+echo
+echo "List of active users:"
+users
+while :
+do
+	echo "Which user to delete?"
+	read user
+	if [ $user != 'root' ]
+	then
+		break
+	else
+		echo "Don't be silly..."
+	fi
+done
+
+read -p "Deleting active user will leave the system to an inconsistent state. Are you sure? (y/n) " yes_no
+
+if [ $yes_no == 'y' ]
+then
+	sudo userdel -rf $user
+fi
+exit
